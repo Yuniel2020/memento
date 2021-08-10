1f8cdb867b0108c921046320ba8541ea5a1703ab
Yuniel Reyes <yreyes8827@gmail.com>
2021-08-11 00:02:01 +0200

    Third commit (landing page-first part )

--- README.md
+++ README.md
@@ -5,10 +5,8 @@
 #### My project will be a journaling app. It will be distinguished by its simplicity as long as it seeks that the user concentrates only on writing.
 
 TODO
-0. Base setting for presentation logic   
-1. Landing page
-   - 
-2. Login Page
-3. Register Page  
-4. Content Page
+0. Base code for presentation logic of all pages   
+1. Landing page / Login page
+2. Register Page  
+3. Content Page
    - Content page will provide access to
--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__) # Creating the Flask application instance 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    """if method == 'POST':
+        render_template('login.html')"""
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'This page does not exist', 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return 'Internal server error', 500
